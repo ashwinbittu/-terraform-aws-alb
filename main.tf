ce993c3ea5513f330a3ad651650baa9cd7a872e6
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-24 14:39:41 +1000

    Add files via upload

--- main.tf
+++ main.tf
@@ -57,6 +57,7 @@ resource "aws_lb" "this" {
   }
 }
 
+
 resource "aws_lb_target_group" "main" {
   count = local.create_lb ? length(var.target_groups) : 0
 
@@ -107,4 +108,26 @@ resource "aws_lb_target_group" "main" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+locals {
+  target_group_attachments = merge(flatten([
+    for index, group in var.target_groups : [
+      for k, targets in group : {
+        for target_key, target in targets : join(".", [index, target_key]) => merge({ tg_index = index }, target)
+      }
+      if k == "targets"
+    ]
+  ])...)
+}
+
+resource "aws_lb_target_group_attachment" "this" {
+  for_each = { for k, v in local.target_group_attachments : k => v if local.create_lb }
+
+  target_group_arn  = aws_lb_target_group.main[each.value.tg_index].arn
+  target_id         = each.value.target_id
+  port              = lookup(each.value, "port", null)
+  availability_zone = lookup(each.value, "availability_zone", null)
+
+  #depends_on = [aws_lambda_permission.lb]
 }
