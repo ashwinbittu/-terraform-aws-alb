f0fa4d6454e678d876c1c56bbd0a785b9f6d722a
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-24 12:21:58 +1000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,117 @@
+locals {
+  create_lb = var.create_lb
+}
+
+resource "aws_lb" "this" {
+  count = local.create_lb ? 1 : 0
+
+  name        = var.name
+
+  load_balancer_type = var.load_balancer_type
+  internal           = var.internal
+  security_groups    = var.security_groups
+  subnets            = var.aws_subnet_ids  
+
+  idle_timeout                     = var.idle_timeout
+  enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
+  enable_deletion_protection       = var.enable_deletion_protection
+  enable_http2                     = var.enable_http2
+  ip_address_type                  = var.ip_address_type
+  drop_invalid_header_fields       = var.drop_invalid_header_fields
+  enable_waf_fail_open             = var.enable_waf_fail_open
+  desync_mitigation_mode           = var.desync_mitigation_mode
+
+  dynamic "access_logs" {
+    for_each = length(keys(var.access_logs)) == 0 ? [] : [var.access_logs]
+
+    content {
+      enabled = try(access_logs.value.enabled, try(access_logs.value.bucket, null) != null)
+      bucket  = try(access_logs.value.bucket, null)
+      prefix  = try(access_logs.value.prefix, null)
+    }
+  }
+
+  dynamic "subnet_mapping" {
+    for_each = var.subnet_mapping
+
+    content {
+      subnet_id            = subnet_mapping.value.subnet_id
+      allocation_id        = lookup(subnet_mapping.value, "allocation_id", null)
+      private_ipv4_address = lookup(subnet_mapping.value, "private_ipv4_address", null)
+      ipv6_address         = lookup(subnet_mapping.value, "ipv6_address", null)
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    var.lb_tags,
+    {
+      Name = var.name != null ? var.name : var.name_prefix
+    },
+  )
+
+  timeouts {
+    create = var.load_balancer_create_timeout
+    update = var.load_balancer_update_timeout
+    delete = var.load_balancer_delete_timeout
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  count = local.create_lb ? length(var.target_groups) : 0
+
+  name        = lookup(var.target_groups[count.index], "name", null)
+
+  vpc_id           = var.vpc_id
+  port             = lookup(var.target_groups[count.index], "backend_port", null)
+  protocol         = lookup(var.target_groups[count.index], "backend_protocol", null) != null ? upper(lookup(var.target_groups[count.index], "backend_protocol")) : null
+  protocol_version = lookup(var.target_groups[count.index], "protocol_version", null) != null ? upper(lookup(var.target_groups[count.index], "protocol_version")) : null
+  target_type      = lookup(var.target_groups[count.index], "target_type", null)
+
+  dynamic "health_check" {
+    for_each = length(keys(lookup(var.target_groups[count.index], "health_check", {}))) == 0 ? [] : [lookup(var.target_groups[count.index], "health_check", {})]
+
+    content {
+      enabled             = lookup(health_check.value, "enabled", null)
+      interval            = lookup(health_check.value, "interval", null)
+      path                = lookup(health_check.value, "path", null)
+      port                = lookup(health_check.value, "port", null)
+      healthy_threshold   = lookup(health_check.value, "healthy_threshold", null)
+      unhealthy_threshold = lookup(health_check.value, "unhealthy_threshold", null)
+      timeout             = lookup(health_check.value, "timeout", null)
+      protocol            = lookup(health_check.value, "protocol", null)
+      matcher             = lookup(health_check.value, "matcher", null)
+    }
+  }
+
+  dynamic "stickiness" {
+    for_each = length(keys(lookup(var.target_groups[count.index], "stickiness", {}))) == 0 ? [] : [lookup(var.target_groups[count.index], "stickiness", {})]
+
+    content {
+      enabled         = lookup(stickiness.value, "enabled", null)
+      cookie_duration = lookup(stickiness.value, "cookie_duration", null)
+      type            = lookup(stickiness.value, "type", null)
+      cookie_name     = lookup(stickiness.value, "cookie_name", null)
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    var.target_group_tags,
+    lookup(var.target_groups[count.index], "tags", {}),
+    {
+      "Name" = lookup(var.target_groups[count.index], "name", lookup(var.target_groups[count.index], "name_prefix", ""))
+    },
+  )
+
+  tags = {
+    Name = "${var.app_name}-elb"
+    environment  = var.app_env
+    appname = var.app_name
+    appid = var.app_id
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
