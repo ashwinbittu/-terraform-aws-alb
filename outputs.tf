7d7ad57e5d290bd468492db884983847bb11cca4
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-27 18:08:56 +1000

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 output "lb_id" {
   description = "The ID and ARN of the load balancer we created."
-  value       = aws_lb.this.id
+  value       = aws_lb.this[0].id
 }
 
 output "lb_arn" {
   description = "The ID and ARN of the load balancer we created."
-  value       = aws_lb.this.arn
+  value       = aws_lb.this[0].arn
 }
 
 output "lb_dns_name" {
   description = "The DNS name of the load balancer."
-  value       = aws_lb.this.dns_name
+  value       = aws_lb.this[0].dns_name
 }
 
 output "lb_arn_suffix" {
   description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = aws_lb.this.arn_suffix
+  value       = aws_lb.this[0].arn_suffix
 }
 
 output "lb_zone_id" {
   description = "The zone_id of the load balancer to assist with creating DNS records."
-  value       = aws_lb.this.zone_id
+  value       = aws_lb.this[0].zone_id
 }
 
 output "target_group_arns" {
   description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-  value       = aws_lb_target_group.main.arn
+  value       = aws_lb_target_group.main[0].arn
 }
 
 output "target_group_arn_suffixes" {
   description = "ARN suffixes of our target groups - can be used with CloudWatch."
-  value       = aws_lb_target_group.main.arn_suffix
+  value       = aws_lb_target_group.main[0].arn_suffix
 }
 
 output "target_group_names" {
   description = "Name of the target group. Useful for passing to your CodeDeploy Deployment Group."
-  value       = aws_lb_target_group.main.name
+  value       = aws_lb_target_group.main[0].name
 }
 
 
