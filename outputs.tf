3e8d4b79dfc78441a4570446e38fca94294ed084
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-27 18:02:54 +1000

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,54 @@
 output "lb_id" {
   description = "The ID and ARN of the load balancer we created."
-  value       = concat(aws_lb.this.*.id, [""])[0]
+  value       = aws_lb.this.id
 }
 
 output "lb_arn" {
   description = "The ID and ARN of the load balancer we created."
-  value       = concat(aws_lb.this.*.arn, [""])[0]
+  value       = aws_lb.this.arn
 }
 
 output "lb_dns_name" {
   description = "The DNS name of the load balancer."
-  value       = concat(aws_lb.this.*.dns_name, [""])[0]
+  value       = aws_lb.this.dns_name
 }
 
 output "lb_arn_suffix" {
   description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = concat(aws_lb.this.*.arn_suffix, [""])[0]
+  value       = aws_lb.this.arn_suffix
 }
 
 output "lb_zone_id" {
   description = "The zone_id of the load balancer to assist with creating DNS records."
-  value       = concat(aws_lb.this.*.zone_id, [""])[0] 
+  value       = aws_lb.this.zone_id
 }
 
+output "target_group_arns" {
+  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
+  value       = aws_lb_target_group.main.arn
+}
+
+output "target_group_arn_suffixes" {
+  description = "ARN suffixes of our target groups - can be used with CloudWatch."
+  value       = aws_lb_target_group.main.arn_suffix
+}
+
+output "target_group_names" {
+  description = "Name of the target group. Useful for passing to your CodeDeploy Deployment Group."
+  value       = aws_lb_target_group.main.name
+}
+
+
+
 /*
+
+output "target_group_attachments" {
+  description = "ARNs of the target group attachment IDs."
+  value = {
+    for k, v in aws_lb_target_group_attachment.this : k => v.id
+  }
+}
+
 output "http_tcp_listener_arns" {
   description = "The ARN of the TCP and HTTP load balancer listeners created."
   value       = aws_lb_listener.frontend_http_tcp.*.arn
@@ -45,31 +70,4 @@ output "https_listener_ids" {
   value       = aws_lb_listener.frontend_https.*.id
 }
 */
-
-output "target_group_arns" {
-  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-  value       = aws_lb_target_group.main.*.arn
-}
-
-output "target_group_arn_suffixes" {
-  description = "ARN suffixes of our target groups - can be used with CloudWatch."
-  value       = aws_lb_target_group.main.*.arn_suffix
-}
-
-output "target_group_names" {
-  description = "Name of the target group. Useful for passing to your CodeDeploy Deployment Group."
-  value       = aws_lb_target_group.main.*.name
-}
-
-/*
-
-output "target_group_attachments" {
-  description = "ARNs of the target group attachment IDs."
-  value = {
-    for k, v in aws_lb_target_group_attachment.this : k => v.id
-  }
-}
-
-*/
-
 
